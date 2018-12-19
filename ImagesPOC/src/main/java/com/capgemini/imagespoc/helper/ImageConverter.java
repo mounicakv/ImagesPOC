0a0d286170c01a1f32501b7aac024e55025fc5ad
mounicakv <mounica.kv@gmail.com>
2018-12-19 07:52:32 +0530

    Get Requests Completed with images download from Resource file and Internet

--- ImagesPOC/src/main/java/com/capgemini/imagespoc/helper/ImageConverter.java
+++ ImagesPOC/src/main/java/com/capgemini/imagespoc/helper/ImageConverter.java
@@ -6,6 +6,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +18,33 @@ public class ImageConverter {
     */
    public byte[] getImageInBytes(File file) throws IOException {
 	 	return FileUtils.readFileToByteArray(file);
+   }
+   
+   /**
+    * Zip the files which are available as part of source files
+    */
+   public byte[] zipFiles(String[] files) throws IOException {
+       ByteArrayOutputStream baos = new ByteArrayOutputStream();
+       ZipOutputStream zos = new ZipOutputStream(baos);
+       byte bytes[] = new byte[2048];
+       for (String fileName : files) {
+       	File f = new ClassPathResource(fileName).getFile();
+           FileInputStream fis = new FileInputStream(f);
+           BufferedInputStream bis = new BufferedInputStream(fis);
+           zos.putNextEntry(new ZipEntry(fileName));
+           int bytesRead;
+           while ((bytesRead = bis.read(bytes)) != -1) {
+               zos.write(bytes, 0, bytesRead);
+           }
+           zos.closeEntry();
+           bis.close();
+           fis.close();
+       }
+       zos.flush();
+       baos.flush();
+       zos.close();
+       baos.close();
+       return baos.toByteArray();
    }
    
    /**
--- ImagesPOC/src/main/java/com/capgemini/imagespoc/service/ImageServiceImpl.java
+++ ImagesPOC/src/main/java/com/capgemini/imagespoc/service/ImageServiceImpl.java
@@ -53,7 +53,7 @@ public class ImageServiceImpl {
 	 */
 	public byte[] getImagesFromResource() throws IOException{
 		String[] files = {"img1.jpeg", "img2.jpeg"};
-		byte[] output = zipFiles(files);
+		byte[] output =imageConverter.zipFiles(files);
 		return output;
 	}
 	
@@ -97,7 +97,7 @@ public class ImageServiceImpl {
 		}
 		
 		String[] files = {"img1.jpeg", "img2.jpeg"};
-		byte[] output = zipFiles(files);
+		byte[] output =imageConverter.zipFiles(files);
 		return output;
 	}
 	
@@ -127,31 +127,6 @@ public class ImageServiceImpl {
 		logger.info("Image file written to stream...");
 		}
 	
-	/**
-     * Zip the files which are available as part of source files
-     */
-    private byte[] zipFiles(String[] files) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ZipOutputStream zos = new ZipOutputStream(baos);
-        byte bytes[] = new byte[2048];
-        for (String fileName : files) {
-        	File f = new ClassPathResource(fileName).getFile();
-            FileInputStream fis = new FileInputStream(f);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            zos.putNextEntry(new ZipEntry(fileName));
-            int bytesRead;
-            while ((bytesRead = bis.read(bytes)) != -1) {
-                zos.write(bytes, 0, bytesRead);
-            }
-            zos.closeEntry();
-            bis.close();
-            fis.close();
-        }
-        zos.flush();
-        baos.flush();
-        zos.close();
-        baos.close();
-        return baos.toByteArray();
-    }
+	
 	
 }
