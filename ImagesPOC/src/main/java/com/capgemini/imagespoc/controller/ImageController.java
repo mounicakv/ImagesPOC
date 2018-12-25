c4db9cf7483076cb26d4bd9fa9f1e80c688ee2d0
mounicakv <mounica.kv@gmail.com>
2018-12-25 20:22:36 +0530

    Changes done for configuring required images from Internet

--- ImagesPOC/src/main/java/com/capgemini/imagespoc/controller/ImageController.java
+++ ImagesPOC/src/main/java/com/capgemini/imagespoc/controller/ImageController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ import com.capgemini.imagespoc.service.ImageServiceImpl ;
 /**
  * Handles requests for the Images service.
  */
-@RestController
+@RestController /*(value="/images")*/
 public class ImageController extends BaseController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
@@ -54,5 +55,23 @@ public class ImageController extends BaseController {
         sos.write(images);
         sos.flush();
 	}
+	
+	@RequestMapping(value = "/images/retrieveCountableImagesFromInternet/count/{countID}", method = RequestMethod.GET, produces="application/zip")
+	public void getRequiredImagesFromInternet(@RequestParam String keyword,@PathVariable int countID, HttpServletResponse response) throws Exception{
+		logger.info("getAllImagesFromInternet");
+		byte[] images = null;
+		logger.info("Keyword passed in input " + keyword);
+		logger.info("Count of images to be retrieved :"+ countID);
+		
+		images =  imageService.getRequiredImagesFromInternet(keyword,countID);
+		ServletOutputStream sos = response.getOutputStream();
+        response.setContentType("application/zip");
+        response.setHeader("Content-Disposition", "attachment; filename=DATA.ZIP");
+        sos.write(images);
+        sos.flush();
+	}
+
+	
+	
 }
 	
--- ImagesPOC/src/main/java/com/capgemini/imagespoc/service/ImageServiceImpl.java
+++ ImagesPOC/src/main/java/com/capgemini/imagespoc/service/ImageServiceImpl.java
@@ -1,14 +1,8 @@
 package com.capgemini.imagespoc.service;
 
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,8 +10,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import javax.imageio.ImageIO;
 
@@ -25,24 +17,19 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.capgemini.imagespoc.controller.ImageController;
-import com.capgemini.imagespoc.dto.Image;
 import com.capgemini.imagespoc.dto.ImageList;
 import com.capgemini.imagespoc.helper.ImageConverter;
-import java.util.stream.Collectors;
 
 @Service
 public class ImageServiceImpl {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
-	
+
 	@Autowired
 	ImageConverter imageConverter;
 
@@ -51,82 +38,128 @@ public class ImageServiceImpl {
 	 * @return
 	 * @throws Exception
 	 */
-	public byte[] getImagesFromResource() throws IOException{
-		String[] files = {"img1.jpeg", "img2.jpeg"};
-		byte[] output =imageConverter.zipFiles(files);
+	public byte[] getImagesFromResource() throws IOException {
+		String[] files = { "img1.jpeg", "img2.jpeg" };
+		byte[] output = imageConverter.zipFiles(files);
 		return output;
 	}
-	
-	public byte[] getImagesFromInternet(String keyword) throws IOException,MalformedURLException{
+
+	public byte[] getImagesFromInternet(String keyword) throws IOException, MalformedURLException {
 		RestTemplate restTemplate = new RestTemplate();
-		String resourceURL
-		  = "http://www.splashbase.co/api/v1/images/search?query={queryparam}";
-		Map<String,String> urlVariables = new HashMap<String, String>();
+		String resourceURL = "http://www.splashbase.co/api/v1/images/search?query={queryparam}";
+		Map<String, String> urlVariables = new HashMap<String, String>();
 		urlVariables.put("queryparam", keyword);
-		ImageList images
-		  = restTemplate.getForObject(resourceURL, ImageList.class, urlVariables);
-		//String responseString = response.getBody();
-		if(null != images && !CollectionUtils.isEmpty(images.getImages())) {
-	logger.info("Image count based on keyword is" + images.getImages().size());	
-			//Iterate these objects using java8 streams
-			//Call the url present in url attribute 
+		ImageList images = restTemplate.getForObject(resourceURL, ImageList.class, urlVariables);
+		// String responseString = response.getBody();
+		if (null != images && !CollectionUtils.isEmpty(images.getImages())) {
+			logger.info("Image count based on keyword is" + images.getImages().size());
+			// Iterate these objects using java8 streams
+			// Call the url present in url attribute
 			images.getImages().stream().forEach(img -> {
 				try {
-					invokeImage(img.getUrl(),img.getId());
+					invokeImage(img.getUrl(), img.getId());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			});
 
-			String[] extensions = {"jpg"};
+			String[] extensions = { "jpg" };
 			List<String> fileNames = new ArrayList<String>();
 			File directory = new File("D://artifacts");
 			Iterator itr = FileUtils.iterateFiles(directory, extensions, true);
-			while(itr.hasNext()) {
-				File f = (File)itr.next();
+			while (itr.hasNext()) {
+				File f = (File) itr.next();
 				fileNames.add(f.getName());
 			}
-			byte[] outputFromDirectory = imageConverter.zipFiles(directory,fileNames);
+			byte[] outputFromDirectory = imageConverter.zipFiles(directory, fileNames);
 			FileUtils.cleanDirectory(directory);
 			return outputFromDirectory;
 		}
-		
+
 		else {
 			logger.info("No images available");
 		}
-		
-		String[] files = {"img1.jpeg", "img2.jpeg"};
-		byte[] output =imageConverter.zipFiles(files);
+
+		String[] files = { "img1.jpeg", "img2.jpeg" };
+		byte[] output = imageConverter.zipFiles(files);
 		return output;
 	}
-	
-	public void invokeImage(String imageUrl,int id) throws IOException{
+
+	public void invokeImage(String imageUrl, int id) throws IOException {
 		logger.info("Inside Invoke Image method...");
-		logger.info("url of image :" +imageUrl);
-		logger.info("Id of image :"+id);
+		logger.info("url of image :" + imageUrl);
+		logger.info("Id of image :" + id);
 		URL url = new URL(imageUrl);
 		BufferedImage img = ImageIO.read(url);
-		File file = new File("D://artifacts/" + "File_"+id+".jpg");
-		//file.getParentFile().mkdirs();
+		File file = new File("D://artifacts/" + "File_" + id + ".jpg");
+		// file.getParentFile().mkdirs();
 		ImageIO.write(img, "jpg", file);
-		
-		/*String destinationFile=null;
-		OutputStream os = new FileOutputStream(destinationFile);
-		
-		
-		 byte[] b = new byte[2048];
-		    int length;
 
-		    while ((length = is.read(b)) != -1) {
-		        os.write(b, 0, length);
-		    }
-		      is.close();
-		    os.close();
-		*/
+		/*
+		 * String destinationFile=null; OutputStream os = new
+		 * FileOutputStream(destinationFile);
+		 * 
+		 * 
+		 * byte[] b = new byte[2048]; int length;
+		 * 
+		 * while ((length = is.read(b)) != -1) { os.write(b, 0, length); } is.close();
+		 * os.close();
+		 */
 		logger.info("Image file written to stream...");
+	}
+
+	public byte[] getRequiredImagesFromInternet(String keyword, int count) throws IOException, MalformedURLException {
+		RestTemplate restTemplate = new RestTemplate();
+		String resourceURL = "http://www.splashbase.co/api/v1/images/search?query={queryparam}";
+		Map<String, String> urlVariables = new HashMap<String, String>();
+		urlVariables.put("queryparam", keyword);
+		ImageList images = restTemplate.getForObject(resourceURL, ImageList.class, urlVariables);
+		// String responseString = response.getBody();
+		if (null != images && !CollectionUtils.isEmpty(images.getImages())) {
+			logger.info("Image count based on keyword is" + images.getImages().size());
+			logger.info("Required images by user  :" + count);
+			// List<Image> ImagesBasedOnCount = new ArrayList<>();
+			if (images.getImages().size() <= count) {
+				logger.info("More than required images are loaded...needs processing");	
+				images.getImages().stream().forEach(img -> {
+					try {
+						invokeImage(img.getUrl(), img.getId());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				});
+			} else {
+				logger.info("Total images available :" + images.getImages().size());
+				logger.info("Images are available but lesser than required");
+				images.getImages().stream().limit(count).forEach(img -> {
+					try {
+						invokeImage(img.getUrl(), img.getId());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				});
+			}}
+
+		String[] extensions = { "jpg" };
+		List<String> fileNames = new ArrayList<String>();
+		File directory = new File("D://artifacts");
+		Iterator itr = FileUtils.iterateFiles(directory, extensions, true);
+		while(itr.hasNext())
+		{
+			File f = (File) itr.next();
+			fileNames.add(f.getName());
 		}
-	
-	
-	
+		byte[] outputFromDirectory = imageConverter.zipFiles(directory,
+				fileNames);
+		FileUtils.cleanDirectory(directory);
+		return outputFromDirectory;
+	}
+
+
+
+
+
 }
